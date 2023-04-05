14204a2131577bc1523b04005ce3cb6058585c7d
Test <m-mik@ro.ru>
2023-04-06 00:06:35 +0300

    Выполнил

--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -1,6 +1,6 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+import { default as Game, GameSavingData, readGameSaving, writeGameSaving } from './game.js'
+const loadGame = readGameSaving();
+const saveGame = writeGameSaving();
+const game = new Game();
+game.start();
+console.log('работает')
--- ci-template/webpack.config.js
+++ ci-template/webpack.config.js
@@ -1,11 +1,12 @@
-
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  target: "web",
   output: {
     path: path.resolve(__dirname, 'dist'),
+    chunkFormat: "commonjs"
   },
   module: {
     rules: [
