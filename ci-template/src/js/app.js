3a4158a595f8eaddc3e755c26101d40257d229b8
Test <m-mik@ro.ru>
2023-05-14 22:23:33 +0300

    0

--- ci-template/src/js/app.js
+++
@@ -1,6 +0,0 @@
-import { default as Game, GameSavingData, readGameSaving, writeGameSaving } from './game.js'
-const loadGame = readGameSaving();
-const saveGame = writeGameSaving();
-const game = new Game();
-game.start();
-console.log('работает')
